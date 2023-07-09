4f1c4fe592b53fcc65712e21f648a238c1b7a9f3
MerinSusanJacob <129526220+MerinSusanJacob@users.noreply.github.com>
2023-07-09 16:22:35 +0930

    move BrowserRouter to App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import Form from './components/Form';
@@ -9,10 +9,12 @@ function App() {
   return (
     <div className="App">
       <Header/>
+      <BrowserRouter>
       <Routes>
         <Route path="/home" element={<Home/>}/>
         <Route path="/form" element={<Form/>}/>
       </Routes>
+      </BrowserRouter>
     </div>
   );
 }
